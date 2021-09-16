4be44d666ab0ce8282e7e8d873d38caba67d525a
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2021-09-16 11:34:22 -0600

    add method to check if char is delimiter or not

--- C_Examples/Assignment 1/tokenizer.c
+++ C_Examples/Assignment 1/tokenizer.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-char** tokenizer(char* ptr, int str_size) {
-    char** ptr_of_ptr;
-    int c;
-    for(c = 0; c<str_size; c++) {
-        ptr++; 
-    }
-    return ptr_of_ptr;
-}
+char** tokenizer(char* ptr, int str_size);
+_Bool is_delimiter(char* c);
 
 int main()
 {
     char str[] = "Hello, world!";
     char** tokens = tokenizer(str, sizeof(str));
     return(0);
+}
+
+char** tokenizer(char* ptr, int str_size) {
+
+    char** ptr_of_ptr;
+    int c;
+
+    for(c = 0; c<str_size; c++) {
+        bool isspace = is_delimiter(ptr);
+        ptr++; 
+    }
+
+    return ptr_of_ptr;
+}
+
+bool is_delimiter(char* c) {
+    return (*c) == ' ';
 }
