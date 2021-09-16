a24d9a0679bd34175b7a21b01c34fc027e6c7081
Oswaldo Escobedo <oescobedo@Oswaldos-MacBook-Air.local>
2021-09-16 11:17:45 -0600

    add tokenizer method and traverse word

--- C_Examples/Assignment 1/tokenizer.c
+++ C_Examples/Assignment 1/tokenizer.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+char** tokenizer(char* ptr, int str_size) {
+    char** ptr_of_ptr;
+    int c;
+    for(c = 0; c<str_size; c++) {
+        ptr++;
+    }
+    return ptr_of_ptr;
+}
+
 int main()
 {
+    char str[] = "Hello, world!";
+    char** tokens = tokenizer(str, sizeof(str));
     return(0);
 }
