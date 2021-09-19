41384ac91c8e9f82ee292ed002b62aab4cb898a4
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2021-09-19 15:14:13 -0600

    add methods to get number of tokens and check if a char is a whitespace char or not

--- C_Examples/Assignment 1/tokenizer.c
+++ C_Examples/Assignment 1/tokenizer.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-char** tokenizer(char* ptr, int str_size);
-_Bool is_delimiter(char* c);
+#include "headers.h"
 
 int main()
 {
     char str[] = "Hello, world!";
-    char** tokens = tokenizer(str, sizeof(str));
+    char** tokens = tokenize(str);
     return(0);
 }
+ 
+char** tokenize(char* str) {
 
-char** tokenizer(char* ptr, int str_size) {
+    int num_tokens = count_tokens(str);
 
-    char** ptr_of_ptr;
-    int c;
+    char** tokens = malloc((num_tokens+1) * sizeof(char*));
 
-    for(c = 0; c<str_size; c++) {
-        bool isspace = is_delimiter(ptr);
-        ptr++;  
+    int w;
+
+    for(w = 0; w<num_tokens; w++) {
+        printf("%d",w);
     }
 
-    return ptr_of_ptr;
+    return tokens;
 }
 
-bool is_delimiter(char* c) {
-    return (*c) == ' ';
+int count_tokens(char* str) {
+    int num_tokens = 0;
+    char *ptr = str;
+    while ((*ptr) != '\0') {
+        if (delim_character((*ptr))) {
+            num_tokens++;
+        } 
+        ptr++;
+    }
+    return num_tokens+1;
+}
+
+bool delim_character(char c) {
+    return c == ' ' || c == '\t';
 }
