1ea337d70f845c9e10b1d23d76613d1fbba23c0b
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2021-09-20 00:06:21 -0600

    add logic to get the start and end of a word

--- C_Examples/Assignment 1/tokenizer.c
+++ C_Examples/Assignment 1/tokenizer.c
@@ -13,19 +13,35 @@ int main() {
     return(0);
 }
  
-char** tokenize(char* str) {
+char** tokenize(char* ptr_str) {
 
-    int num_tokens = count_tokens(str);
+    int num_tokens = count_tokens(ptr_str);
 
     char** tokens = malloc((num_tokens+1) * sizeof(char*));
 
     int w;
 
     for(w = 0; w<num_tokens; w++) {
-        printf("%d",w);
+        char* start_char = word_start(ptr_str);
+        char* end_char = word_end(start_char);
+        ptr_str = end_char;
     }
 
     return tokens;
+}
+
+char* word_start(char* ptr_str) {
+    while (delim_character((*ptr_str))) { // until there are whitespaces
+        ptr_str++;
+    }
+    return ptr_str;
+}
+
+char* word_end(char* ptr_str) {
+    while (!delim_character((*ptr_str))) { // until there are non-whitespaces
+        ptr_str++;
+    }
+    return ptr_str;
 }
 
 int count_tokens(char* str) {
