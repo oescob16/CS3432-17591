3138fcb61d7443b828a8606d0f052b4eee08d596
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2021-10-04 18:17:10 -0600

    add logic to handle and manipulate user input

--- C_Examples/Assignment 2/riscv.c
+++ C_Examples/Assignment 2/riscv.c
@@ -4,13 +4,11 @@
 #include <stdint.h> // use guaranteed 64-bit integers
 #include "tokenizer.h" // Create header file and reference that
 #include "memory.h" // built-in functions to read and write to a specific file
+#include "riscv.h"
+#include "string.h"
+#include "mnemonic.h"
 
 int32_t* reg; // Array of 32 32-bit registers
-
-void print_regs();
-void init_regs();
-bool interpret(char* instr);
-void write_read_demo();
 
 void print_regs(){
 	int col_size = 10;
@@ -34,7 +32,9 @@ void init_regs(){
 		reg[i] = i;
 }
 
-
+char* tokenize(char* str, const char delim) {
+	return strtok(str, delim);
+}
 
 /**
  * Fill out this function and use it to read interpret user input to execute RV64 instructions.
@@ -42,9 +42,83 @@ void init_regs(){
  * as a parameter to this function.
  */
 bool interpret(char* instr){
+	char* tokens = tokenize(instr, " ");
+
+	int mnemonic = interpret_helper(tokens[0]);
+	bool has_worked;
+
+	switch (mnemonic) {
+		case 0:
+			has_worked = true;
+			break;
+		case 1:
+			has_worked = true;
+			break;
+		case 2:
+			has_worked = true;
+			break;
+		case 3:
+			has_worked = true;
+			break;
+		default:
+			has_worked = false;
+			break;
+	}
+
+	return has_worked;
+}
+
+/* 
+	LW -> 0
+	SW -> 1
+	ADD -> 2
+	ADDI -> 3
+*/
+int interpret_helper(char* mnemonic) {
+	if (is_str_equal(mnemonic, "LW")) {
+		return 0;
+	} else if (is_str_equal(mnemonic, "SW")) {
+		return 1;
+	} else if (is_str_equal(mnemonic, "ADD")) {
+		return 2;
+	} else if (is_str_equal(mnemonic, "ADDI")) {
+		return 3;
+	}
+
+	return -1;
+}
+
+bool is_str_equal(char* str1, char* str2) {
+	int size_str1 = str_size(str1);
+	int size_str2 = str_size(str2);
+
+	if (size_str1 != size_str2) {
+		return false;
+	} 
+
+	int c;
+	for (c = 0; c < size_str1; c++) {
+		if ((*str1) != (*str2)) {
+			return false;
+		}
+		str1++;
+		str2++;
+	}
 	return true;
 }
 
+int str_size(char* str) {
+	int size = 0;
+	while ((*str) != '\0') {
+        size++;
+        str++;
+    }
+    return size;
+}
+
+int atoi(char* str) {
+	return 0;
+}
 
 /**
  * Simple demo program to show the usage of read_address() and write_address() found in memory.c
@@ -77,9 +151,9 @@ int main(){
 	//print_regs();
 
 	// Below is a sample program to a write-read. Overwrite this with your own code.
-	write_read_demo();
+	//write_read_demo();
 
-	/*printf(" RV32 Interpreter.\nType RV32 instructions. Use upper-case letters and space as a delimiter.\nEnter 'EOF' character to end program\n");
+	printf(" RV32 Interpreter.\nType RV32 instructions. Use upper-case letters and space as a delimiter.\nEnter 'EOF' character to end program\n");
 
 	char* instruction = malloc(1000 * sizeof(char));
 	bool is_null = false;
@@ -95,7 +169,6 @@ int main(){
 	}
 
 	printf("Good bye!\n");
-	*/
 
 	return 0;
 }
