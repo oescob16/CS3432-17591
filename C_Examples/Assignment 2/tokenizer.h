cb4b54ad983147165bcbc5fa62ed2df0144f93f5
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2021-10-04 18:16:37 -0600

    add new methods to manipulate tokens

--- C_Examples/Assignment 2/tokenizer.h
+++ C_Examples/Assignment 2/tokenizer.h
@@ -1,1 +1,7 @@
-char** tokenize(char* str, const char delim);
+char* tokenize(char* str, const char delim);
+
+bool is_str_equal(char* str1, char* str2);
+
+int str_size(char* str);
+
+int atoi(char* str);
